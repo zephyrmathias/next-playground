984152507ab48260a5484604b9a0dc0d9447d04b
Zephyr Mathias <wuttichai.junsoontraporn@gmail.com>
2020-11-25 01:40:17 +0700

    test pr
    desc

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 export default function Homepage({ locale }) {
-  return <div>homepage - locale {locale}</div>
+  return (
+    <div>
+      homepage - locale {locale}
+      <div>
+        <Link href='/test'>test</Link>
+      </div>
+    </div>
+  )
 }
 
 export async function getStaticProps({ locale }) {
